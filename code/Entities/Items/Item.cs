1c2132cc0431d24b082bb2d2ab8db36d47111da9
Jomar Milan <jomarm@jomarm.tech>
2023-08-22 22:19:06 -0700

    Implement base item classes
    Implemented item and ranged weapon base classes in this commit.

---
+++ code/Entities/Items/Item.cs
@@ -0,0 +1,91 @@
+﻿using Sandbox;
+
+namespace BoxBusters.Entities.Items
+{
+	public class Item : AnimatedEntity
+	{
+		protected BaseViewModel ViewModelEntity { get; private set; }
+		
+		protected virtual string ViewModelPath => null;
+
+		public virtual bool CanCarry => true;
+
+		public Entity EffectEntity
+		{
+			get
+			{
+				if ( ViewModelEntity.IsValid() && IsFirstPersonMode )
+				{
+					return ViewModelEntity;
+				}
+
+				return this;
+			}
+		}
+
+		public override void Spawn()
+		{
+			base.Spawn();
+
+			PhysicsEnabled = true;
+			UsePhysicsCollision = true;
+			EnableHideInFirstPerson = true;
+			EnableShadowInFirstPerson = true;
+			
+			Tags.Add( "item" );
+		}
+
+		public virtual void OnCarryStart( Entity carrier )
+		{
+			// Don't do anything on the client, let s&box network.
+			if ( Game.IsClient )
+			{
+				return;
+			}
+
+			SetParent( carrier, true );
+			Owner = carrier;
+			EnableAllCollisions = false;
+			EnableDrawing = false;
+		}
+
+		public virtual void OnCarryDrop( Entity dropper )
+		{
+			EnableDrawing = true;
+
+			if ( IsLocalPawn )
+			{
+				DestroyViewModel();
+				DestroyHudElements();
+
+				CreateViewModel();
+				CreateHudElements();
+			}
+		}
+
+		public void CreateViewModel()
+		{
+			Game.AssertClient();
+
+			if ( string.IsNullOrEmpty( ViewModelPath ) )
+			{
+				return;
+			}
+			
+			ViewModelEntity = new BaseViewModel();
+			ViewModelEntity.Position = Position;
+			ViewModelEntity.Owner = Owner;
+			ViewModelEntity.EnableViewmodelRendering = true;
+			ViewModelEntity.SetModel( ViewModelPath );
+		}
+		
+		public void DestroyViewModel()
+		{}
+		
+		public virtual void CreateHudElements()
+		{}
+		
+		public virtual void DestroyHudElements()
+		{}
+	}
+}
---
+++ code/Entities/Items/RangedWeapon.cs
@@ -0,0 +1,96 @@
+﻿using Sandbox;
+
+namespace BoxBusters.Entities.Items
+{
+	public partial class RangedWeapon : Item
+	{
+		public virtual float PrimaryRate => 5f;
+		
+		public virtual float SecondaryRate => 5f;
+		
+		[Net, Predicted]
+		public TimeSince TimeSincePrimaryAttack { get; private set; }
+		
+		[Net, Predicted]
+		public TimeSince TimeSinceSecondaryAttack { get; private set; }
+
+		public bool CanPrimaryAttack
+		{
+			get
+			{
+				if ( PrimaryRate <= 0 )
+				{
+					return true;
+				}
+				
+				return TimeSincePrimaryAttack >= 1 / PrimaryRate;
+			}
+		}
+		
+		public bool CanSecondaryAttack
+		{
+			get
+			{
+				if ( SecondaryRate <= 0 )
+				{
+					return true;
+				}
+				
+				return TimeSinceSecondaryAttack >= 1 / SecondaryRate;
+			}
+		}
+		
+		public virtual void Reload() {}
+
+		public virtual void AttackPrimary()
+		{
+			TimeSincePrimaryAttack = 0;
+		}
+		
+		public virtual void AttackSecondary()
+		{
+			TimeSinceSecondaryAttack = 0;
+		}
+
+		public override void Simulate( IClient cl )
+		{
+			if ( !Owner.IsValid() )
+			{
+				return;
+			}
+
+			if ( Input.Pressed( "reload" ) )
+			{
+				Reload();
+			}
+			
+			if ( CanPrimaryAttack && Input.Down( "attack1" ) )
+			{
+				AttackPrimary();
+			}
+			
+			if (CanSecondaryAttack && Input.Down( "attack2" ) )
+			{
+				AttackSecondary();
+			}
+		}
+		
+		public virtual TraceResult TraceBullet( Vector3 start, Vector3 end, float radius = 2f )
+		{
+			bool underwater = Trace.TestPoint( start, "water" );
+
+			Trace trace = Trace.Ray( start, end )
+				.UseHitboxes()
+				.WithAnyTags( "solid", "player", "npc" )
+				.Ignore( this )
+				.Size( radius );
+
+			if ( !underwater )
+			{
+				trace = trace.WithAnyTags( "water" );
+			}
+
+			return trace.Run();
+		}
+	}
+}
